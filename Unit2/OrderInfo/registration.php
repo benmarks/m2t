526e19f42adc0b4a110b416ed7b27cfe89a941a8
Benjamin Marks <bmarks@RMT-LM-001015.microbizz.com>
2017-04-11 09:43:29 +0300

    adding unit 2

---
+++ Unit2/OrderInfo/registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Unit2_OrderInfo',
+    __DIR__
+);
